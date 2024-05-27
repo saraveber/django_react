05200957df7516b7c90fd37af4c41b58d3e8c905
Sara Veber <sara22.veber@gmail.com>
2024-05-27 15:51:49 +0200

    Fix typo in import statement for User model

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,4 +1,4 @@
-from djando.contrib.auth.models import User
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,5 +11,4 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
-    
     
