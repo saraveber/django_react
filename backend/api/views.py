50b8d9051098f73ad077636f1bbe8aeaa3e9113e
Sara Veber <sara22.veber@gmail.com>
2024-05-27 16:56:53 +0200

    feat: Add Note model and serializer
    This commit adds a new `Note` model to the `models.py` file, which includes fields for `title`, `content`, `author`, `created_at`, and `updated_at`. It also adds a `NoteSerializer` class to the `serializer.py` file, which specifies the model and its fields.
    
    The purpose of this change is to introduce a new model and serializer for handling notes in the API.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializer import UserSerializer
+from .serializer import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user) 
 
 
 
-# Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = [AllowAny]
+
+
