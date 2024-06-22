7df18693228cb3e5f2096b0b394f1d65f5923d4d
Sara Veber <sara22.veber@gmail.com>
2024-06-22 12:33:10 +0200

    feat: Update AvailableTerm model to use DateTimeField for start_date and end_date

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Legacy code
 # class NoteAdmin(admin.ModelAdmin):
 class Note(models.Model):
@@ -13,12 +12,11 @@ class Note(models.Model):
     def __str__(self):
         return self.title
 
-
 # Define the AvailableTerm model
 class AvailableTerm(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='available_terms')
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)    
 
     def __str__(self):
