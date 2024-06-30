1180ed65cdc91c2651a4e694742ee650b9ba7e3c
Sara Veber <sara22.veber@gmail.com>
2024-06-30 10:27:41 +0200

    refactor: Remove console.log statements from Calendar and Navigation components

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -10,7 +10,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Navigation() {
     
     const { currUser, role, loading } = useUser();
-    console.log(currUser, role, loading)
 
     return (
         <Navbar key='md' expand='md' className="bg-primary mb-3" variant="dark">
